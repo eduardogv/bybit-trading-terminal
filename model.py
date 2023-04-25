a31c634a4d9059abbb3dc1f3143fd6e72049e2ac
eduardogv <eduardogrados2@gmail.com>
2023-04-25 02:48:40 -0500

    Exception handling
    Send order exception handling

--- model.py
+++ model.py
@@ -8,14 +8,30 @@ class Model():
         self.__place_order = Order()
         self.__calcultor = Calculator()
         self.__position_size = 0
+        self.__coin_size = 0
+        self.__equity = 0
+        self.__entry = 0
+        self.__account_balance = 0
+        
 
-
-    def calculate_pos(self):
-        self.__position_size = self.__calcultor.sl_distance(200, 1, 20, 1.01, entry_price=0.6578 )
-        print (self.__position_size)
-
+    def place_order(self, entry):
+        entry = self.__entry
+        self.__account_balance = 0
+        self.calculate_pos()
 
     
+    def calculate_pos(self):
+
+        # Calculate position values from tuple 
+        self.__position_size, self.__coin_size, self.__equity = self.__calcultor.sl_distance(200, 1, 20, 1.01, entry_price=self.__entry)
+
+    def place_order(self):
+        pass
+        
+
+
+
+
     
     # def place_order(self):
     #     self.__position_size = self.__calcultor.sl_distance(capital= 200 , risk= 1, leverage= 20, sl= 1.01)
--- send_orders.py
+++ send_orders.py
@@ -1,6 +1,7 @@
 
 # Import HTTP from the unified_trading module.
 from pybit.unified_trading import HTTP
+from pybit.exceptions import InvalidRequestError
 
 """
 You can create an authenticated or unauthenticated HTTP session. 
@@ -15,8 +16,8 @@ secret_key_imported = secret_key
 
 class Order():
 
-    def __init__(self):
-        
+    def __init__(self):    
+
         # Authenticated
         self.__session = HTTP(
         testnet=False,
@@ -25,56 +26,78 @@ class Order():
         )
 
         self.__account_balance = "0"
-        self.get_account_balance()
 
 
     def get_account_balance(self):
-        # Get wallet balance of the Unified Trading Account
+        
+        # Get wallet balance of the Normal Trading Account
         self.__account_balance = self.__session.get_wallet_balance(accountType="CONTRACT")
         self.__account_balance = self.__account_balance["result"]["list"][0]["coin"][0]["equity"]
-        print(f"El balance de su cuenta es: {self.__account_balance}.")
+        #print(f"El balance de su cuenta es: {self.__account_balance}.")
         return self.__account_balance
         
-        
-# Acá me doy cuenta que la moneda tiene que tener habilitado el hedge mode para que el positionIdx funcione.
-# Si no tienes habilitado el hedge mode para cierto PAR, no va a funcionar el positionIdx=1, tienes que usar el 0 que es one way position 
-# Tenemos que validar previamente que tipo de trading tienes para la moneda particular, y poder calcular
 # positionIdx :
 #   0 one-way mode position
 #   1 Buy side of hedge-mode position
 #   2 Sell side of hedge-mode position      
 
     def open_long(self, price, tp, sl,  coin="BTCUSDT", order_type="Limit", size=0.001):
+        """
+        positionIdx :
+        0 one-way mode position
+        1 Buy side of hedge-mode position
+        2 Sell side of hedge-mode position 
+        """
         # Place an order on that USDT Perpetual
-        print(self.__session.place_order(
-            category="linear",
-            symbol=coin,
-            side="Buy",
-            orderType=order_type,
-            qty = size,
-            price=price,
-            takeProfit=tp,
-            stopLoss=sl,
-            positionIdx=1
-        ))
+        if tp>price>sl:
+            try:
+                print(self.__session.place_order(
+                    category="linear",
+                    symbol=coin,
+                    side="Buy",
+                    orderType=order_type,
+                    qty = size,
+                    price=price,
+                    takeProfit=tp,
+                    stopLoss=sl,
+                    positionIdx=1
+                ))
+            except InvalidRequestError as e:
+                print(repr(e))
+                print(e)
+        else:
+            print("Burro, el TP debe ser mayor que el entry")
 
 
     def open_short(self, price, tp, sl,  coin="BTCUSDT", order_type="Limit", size=0.001):
-        # Place an order on that USDT Perpetual
-        print(self.__session.place_order(
-            category="linear",
-            symbol=coin,
-            side="Sell",
-            orderType=order_type,
-            qty = size,
-            price=price,
-            takeProfit=tp,
-            stopLoss=sl,
-            positionIdx=2,
-        ))
+        """
+        positionIdx :
+        0 one-way mode position
+        1 Buy side of hedge-mode position
+        2 Sell side of hedge-mode position 
+        """
 
-# prueba = Order()
-# prueba.open_long("0.6578", tp="0.72625", sl="0.65115", coin="IDUSDT", size=301)
+        # Place an order on that USDT Perpetual
+        if sl>price>tp:
+            try:
+                print(self.__session.place_order(
+                    category="linear",
+                    symbol=coin,
+                    side="Sell",
+                    orderType=order_type,
+                    qty = size,
+                    price=price,
+                    takeProfit=tp,
+                    stopLoss=sl,
+                    positionIdx=2,
+                ))
+            except :
+                print("ha ocurrido un error")
+        else:
+            print("burro tus entradas estan mal")
+
+prueba = Order()
+prueba.open_long("0.6578", tp="0.72625", sl="0.65115", coin="IDUSDT", size=301)
 
 """
 prueba = Order()
